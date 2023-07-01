73824989ce1f5500b1cd0d8a8281eed5a95899f8
EGORDYU <egordyuzhev@gmail.com>
2023-06-30 18:29:13 -0600

    config change

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || '',
-  basePath: process.env.BASE_PATH || '',
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
